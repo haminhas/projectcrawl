5970c626b06697b33f2b2081bdba44be1001f566
FedericoKCL <fed.deployment@aol.co.uk>
2015-10-19 15:14:45 +0100

    added methods and paramenters for Player Object

--- Crawl/app/src/main/java/uk/co/ivaylokhr/crawl/Player.java
+++ Crawl/app/src/main/java/uk/co/ivaylokhr/crawl/Player.java
@@ -4,4 +4,29 @@ package uk.co.ivaylokhr.crawl;
  * Created by Hassan on 19/10/2015.
  */
 public class Player {
+    String name ;
+    PlayerCup playerCup;
+    boolean turn ;
+
+    public Player(String name, PlayerCup playerCup) {
+        this.name = name;
+        this.playerCup = playerCup;
+        turn = false;
+    }
+
+    public void setTurn(boolean turn) {
+        this.turn = turn;
+    }
+
+    public boolean getTurn() {
+        return turn;
+    }
+
+    public void increaseScore(int marblesToAdd) {
+        playerCup.addMarbles(marblesToAdd);
+    }
+
+    public int getScore() {
+        return playerCup.getMarbles();
+    }
 }
