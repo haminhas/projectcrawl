c521272232159a0a920f720d4c3a848614824054
Hassan Minhas <Hassan@Hassans-MacBook-Pro.local>
2015-10-20 17:31:30 +0100

    had to manually set the id for all buttons

--- Crawl/app/src/main/java/uk/co/ivaylokhr/crawl/MainActivity.java
+++ Crawl/app/src/main/java/uk/co/ivaylokhr/crawl/MainActivity.java
@@ -44,21 +44,37 @@ public class MainActivity extends AppCompatActivity {
     private Cup[] fillTheArray(){
         cups = new Cup[16];
         cups[0] = (PocketCup) findViewById(R.id.b0);
+        cups[0].setId(0);
         cups[1] = (PocketCup) findViewById(R.id.b1);
+        cups[1].setId(1);
         cups[2] = (PocketCup) findViewById(R.id.b2);
+        cups[2].setId(2);
         cups[3] = (PocketCup) findViewById(R.id.b3);
+        cups[3].setId(3);
         cups[4] = (PocketCup) findViewById(R.id.b4);
+        cups[4].setId(4);
         cups[5] = (PocketCup) findViewById(R.id.b5);
+        cups[5].setId(5);
         cups[6] = (PocketCup) findViewById(R.id.b6);
+        cups[6].setId(6);
         cups[7] = (PlayerCup) findViewById(R.id.b7);
+        cups[7].setId(7);
         cups[8] = (PocketCup) findViewById(R.id.b8);
+        cups[8].setId(8);
         cups[9] = (PocketCup) findViewById(R.id.b9);
+        cups[9].setId(9);
         cups[10] = (PocketCup) findViewById(R.id.b10);
+        cups[10].setId(10);
         cups[11] = (PocketCup) findViewById(R.id.b11);
+        cups[11].setId(11);
         cups[12] = (PocketCup) findViewById(R.id.b12);
+        cups[12].setId(12);
         cups[13] = (PocketCup) findViewById(R.id.b13);
+        cups[13].setId(13);
         cups[14] = (PocketCup) findViewById(R.id.b14);
+        cups[14].setId(14);
         cups[15] = (PlayerCup) findViewById(R.id.b15);
+        cups[15].setId(15);
         return cups;
     }
 }
