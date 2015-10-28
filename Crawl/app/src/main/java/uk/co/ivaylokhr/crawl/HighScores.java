056bbb1c97fcf1ed4d45f2faeed73f50e1eb60f4
sola <solanylander@gmail.com>
2015-10-28 02:29:48 +0000

    forgot to add new files

---
+++ Crawl/app/src/main/java/uk/co/ivaylokhr/crawl/HighScores.java
@@ -0,0 +1,62 @@
+package uk.co.ivaylokhr.crawl;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class HighScores extends AppCompatActivity {
+    private TextView first;
+    private TextView second;
+    private TextView third;
+    private TextView games;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_scores);
+        Log.i("tag", "test");
+        first = (TextView) findViewById(R.id.textView6);
+        second = (TextView) findViewById(R.id.textView10);
+        third = (TextView) findViewById(R.id.textView11);
+        games = (TextView) findViewById(R.id.textView12);
+        SharedPreferences sp = getSharedPreferences("your_prefs", Activity.MODE_PRIVATE);
+        first.setText("1st Place:  " + sp.getInt("first", -1));
+        second.setText("2nd Place: "+sp.getInt("second", -1));
+        third.setText("3rd Place:  " + sp.getInt("third", -1));
+        games.setText("Total Games played: " + sp.getInt("games", -1));
+    }
+
+    public void back(View view) {
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_high_scores, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
