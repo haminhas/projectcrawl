565e9ebf876c7e50865300b896547ad538ebf336
FedericoKCL <fed.deployment@aol.co.uk>
2015-11-16 11:54:42 +0000

    renamed xml bluetooth list file

--- Crawl/app/src/main/java/uk/co/ivaylokhr/crawl/Controller/BluetoothController.java
+++ Crawl/app/src/main/java/uk/co/ivaylokhr/crawl/Controller/BluetoothController.java
@@ -140,7 +140,7 @@ public class BluetoothController extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bluetooth_chat, container, false);
+        return inflater.inflate(R.layout.fragment_devicelist, container, false);
     }
 
     /**
